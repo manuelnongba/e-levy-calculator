2cee0dff637b86de445dab5dc175f35a955ae9a9
manuelnongba <97706492+manuelnongba@users.noreply.github.com>
2022-09-17 23:58:26 +0000

    updated

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 'use-strict';
 const input = document.querySelector('.input');
+const inputM = document.querySelector('.inputm');
 const displayTotal = document.querySelector('.result');
 const submit = document.querySelector('.submit');
+const inputType = document.querySelector('.form__input--type');
+const container = document.querySelector('.form__row');
+const vodafone = document.querySelector('.network__voda');
+const mtn = document.querySelector('.network__mtn');
+const resultField = document.querySelector('.total-display');
+const resultFieldM = document.querySelector('.total-displaym');
 
+console.log(inputType.value);
 displayTotal.textContent = `GH¢ 0`;
 
 input.addEventListener('input', function (e) {
@@ -13,5 +21,24 @@ input.addEventListener('input', function (e) {
     displayTotal.textContent = `GH¢ ${result}`;
   } else {
     displayTotal.textContent = `GH¢ ${amount}`;
+  }
+});
+
+inputType.addEventListener('change', function (e) {
+  e.preventDefault();
+  if (inputType.value === 'vodafone') {
+    vodafone.classList.remove('hidden');
+    mtn.classList.add('hidden');
+    inputType.style.border = '5px solid rgb(208, 52, 52)';
+    input.style.border = '5px solid rgb(208, 52, 52)';
+    resultField.style.border = '1px solid rgb(208, 52, 52)';
+  }
+
+  if (inputType.value === 'MTN') {
+    mtn.classList.remove('hidden');
+    vodafone.classList.add('hidden');
+    inputType.style.border = '5px solid rgb(215, 215, 76)';
+    inputM.style.border = '5px solid rgb(215, 215, 76)';
+    resultFieldM.style.border = '1px solid rgb(215, 215, 76)';
   }
 });
