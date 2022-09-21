ed8c574e1685fa7e96cc1b945140dbdac8bcb558
manuelnongba <97706492+manuelnongba@users.noreply.github.com>
2022-09-21 10:17:24 +0000

    E-Levy Calculator

--- script.js
+++ script.js
@@ -2,20 +2,20 @@
 const input = document.querySelector('.input');
 const inputM = document.querySelector('.input-m');
 const displayTotal = document.querySelector('.result');
-const displayTotalMTN = document.querySelector('.result-m');
+// const displayTotalMTN = document.querySelector('.result-m');
 const resultElevy = document.querySelector('.result-elevy');
-const resultElevyMTN = document.querySelector('.result-elevy-m');
+// const resultElevyMTN = document.querySelector('.result-elevy-m');
 const networkFee = document.querySelector('.network');
-const networkMTN = document.querySelector('.network-m');
+// const networkMTN = document.querySelector('.network-m');
 const inputType = document.querySelector('.form__input--type');
-const vodafone = document.querySelector('.network__voda');
-const mtn = document.querySelector('.network__mtn');
+const network = document.querySelector('.network__voda');
+// const mtn = document.querySelector('.network__mtn');
 const resultField = document.querySelector('.total-display');
-const resultFieldM = document.querySelector('.total-display-m');
+// const resultFieldM = document.querySelector('.total-display-m');
 
-displayTotal.textContent = `GH¢ 0`;
-resultElevy.textContent = `GH¢ 0`;
-networkFee.textContent = `GH¢ 0`;
+displayTotal.textContent = `GH¢ ${0}`;
+resultElevy.textContent = `GH¢ ${0}`;
+networkFee.textContent = `GH¢ ${0}`;
 
 input.addEventListener('input', function (e) {
   e.preventDefault();
@@ -27,31 +27,37 @@ input.addEventListener('input', function (e) {
     resultElevy.textContent = `GH¢ ${elevy}`;
   } else {
     displayTotal.textContent = `GH¢ ${amount}`;
-    resultElevy.textContent = 0;
+    resultElevy.textContent = `GH¢ ${0}`;
   }
 });
 
 inputType.addEventListener('change', function (e) {
   e.preventDefault();
   if (inputType.value === 'vodafone') {
-    vodafone.classList.remove('hidden');
+    input.value = '';
+    displayTotal.textContent = `GH¢ ${0}`;
+    resultElevy.textContent = `GH¢ ${0}`;
+    network.classList.remove('hidden');
     // mtn.classList.add('hidden')
-    inputType.style.border = '5px solid white';
+    inputType.style.border = '3.5px solid white';
     input.style.border = '5px solid white';
-    resultField.style.border = '5px solid white';
+    resultField.style.border = '3.5px solid white';
     document.body.style.backgroundColor = 'rgb(228, 44, 44)';
   }
 
   if (inputType.value === 'MTN') {
-    vodafone.classList.remove('hidden');
+    input.value = '';
+    displayTotal.textContent = `GH¢ ${0}`;
+    resultElevy.textContent = `GH¢ ${0}`;
+    network.classList.remove('hidden');
     // vodafone.classList.add('hidden');
-    inputType.style.border = '5px solid rgb(10, 66, 110)';
-    input.style.border = '5px solid rgb(10, 66, 110)';
-    resultField.style.border = '5px solid rgb(10, 66, 110)';
+    inputType.style.border = '3.5px solid rgb(10, 66, 110)';
+    input.style.border = '3.5px solid rgb(10, 66, 110)';
+    resultField.style.border = '3.5px solid rgb(10, 66, 110)';
     document.body.style.backgroundColor = 'rgb(234, 206, 9)';
   }
-  console.log(inputType.value);
-
-  if (inputType.value === 'select')
-    document.body.backgroundColor = 'rgb(159, 154, 154)';
+  if (inputType.value === 'select') {
+    document.querySelector('.network__voda').classList.add('hidden');
+    document.body.style.backgroundColor = 'rgb(159, 154, 154)';
+  }
 });
